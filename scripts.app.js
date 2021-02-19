0234bd78c2e750244472e72378373ede117eb732
kkaur89 <karenjeetkaurp@gmail.com>
2021-02-19 11:39:13 +0000

    basic grid added

--- scripts.app.js
+++ scripts.app.js
@@ -1,10 +1,34 @@
 function init() {
 
-console.log('content loaded')
+  const grid = document.querySelector('.grid')
+
+  const width = 6
+  const height = 7
+  const cellCount = width * height
+  const cells = []
+
+  let currentPlayer = ['player one', 'player two']
+  console.log('I am current player', currentPlayer)
 
 
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+
+  }
+
+  function startGame(event) {
+    console.log('event', event.target)
+  }
+
+  document.addEventListener('click', startGame)
 
 
+  createGrid()
 
 
 
